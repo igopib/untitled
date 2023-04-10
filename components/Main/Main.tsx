f19ce97befa9dc8fe8eb23da1bc63be4107357fa
Gurpreet Singh <gs109690@gmail.com>
2023-04-10 05:15:38 -0500

    Added Function Component (FC) in the Main component

--- components/Main/Main.tsx
+++ components/Main/Main.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image"
 import Link from "next/link"
+import { FC } from "react"
 
 // Internal Imports
 import Style from "./Main.module.css"
 
-const Main = () => {
+const Main: FC = () => {
   return (
     <>
       <div>Navigation</div>
